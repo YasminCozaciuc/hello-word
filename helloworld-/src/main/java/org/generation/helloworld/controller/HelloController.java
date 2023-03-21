50ddac375e1b921c25483d43b7ed8c59f1bc7828
Yasmin Jannuzzi Cozaciuc <yasmin.cozaciuc@gmail.com>
2023-03-21 12:25:01 -0300

    helloworld

---
+++ helloworld-/src/main/java/org/generation/helloworld/controller/HelloController.java
@@ -0,0 +1,27 @@
+package org.generation.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+//indica para o spring que essa é uma classe controladora
+@RestController
+
+//indica um endpoint para nossa aplicação
+@RequestMapping("/hello")
+
+public class HelloController {
+
+	@GetMapping
+	public String batata() {
+		return "Alô mundão";
+	}
+	
+	@GetMapping("/bsm")
+	public String bsm() {
+		return "Atenção aos detalhes";
+	}
+	
+	
+}
